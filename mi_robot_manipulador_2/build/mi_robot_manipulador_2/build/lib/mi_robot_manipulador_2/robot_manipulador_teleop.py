39e912dc586cc0348b387ba341b5a4d5dfc3358f
Fernandralo <passwd>
2023-04-23 01:16:01 -0500

    Avance2

--- mi_robot_manipulador_2/build/mi_robot_manipulador_2/build/lib/mi_robot_manipulador_2/robot_manipulador_teleop.py
+++ mi_robot_manipulador_2/build/mi_robot_manipulador_2/build/lib/mi_robot_manipulador_2/robot_manipulador_teleop.py
@@ -1,80 +1,66 @@
+#!/usr/bin/env python
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from geometry_msgs.msg import Vector3
-import sys, select, termios, tty
+import math
+import keyboard
 
+class RobotController(Node):
+    def __init__(self, joint_speeds):
+        super().__init__('robot_controller')
+        self.joint_speeds = joint_speeds
+        self.current_joint_positions = [0, 0, 0]
+        self.gripper_open = False
+        self.zone = ""
 
-class TeleopRobot(Node):
+        self.joint_positions_pub = self.create_publisher(Vector3, '/joint_positions', 10)
+        self.gripper_pub = self.create_publisher(String, '/gripper', 10)
 
-    def __init__(self):
-        super().__init__('teleop_robot')
-        self.velocity = {'joint1': 0.0, 'joint2': 0.0, 'joint3': 0.0}
-        self.pub_goal = self.create_publisher(Vector3, '/robot_manipulador_goal', 10)
-        self.pub_zone = self.create_publisher(String, '/robot_manipulador_zone', 10)
-        self.sub_position = self.create_subscription(Vector3, '/robot_manipulador_position', self.position_callback, 10)
+    def update_joint_positions(self, axis_positions):
+        # Calculate new joint positions based on joystick or keyboard input
+        new_joint_positions = []
+        for i in range(len(axis_positions)):
+            joint_pos = self.current_joint_positions[i] + self.joint_speeds[i] * axis_positions[i]
+            joint_pos = max(-math.pi/2, min(math.pi/2, joint_pos))  # Joint limit range
+            new_joint_positions.append(joint_pos)
 
-        self.timer = self.create_timer(0.1, self.timer_callback)
+        # Update current joint positions and publish to topic
+        self.current_joint_positions = new_joint_positions
+        joint_pos_msg = Vector3(x=new_joint_positions[0], y=new_joint_positions[1], z=new_joint_positions[2])
+        self.joint_positions_pub.publish(joint_pos_msg)
 
-        self.fd = sys.stdin.fileno()
-        self.old_settings = termios.tcgetattr(self.fd)
-        tty.setraw(sys.stdin.fileno())
+    def open_gripper(self):
+        # Open gripper with default step size of 2
+        gripper_step = 2
+        self.gripper_open = True
+        gripper_pos = gripper_step
+        gripper_msg = String(data=str(gripper_pos))
+        self.gripper_pub.publish(gripper_msg)
 
-        self.get_logger().info('Teleop robot initialized')
-
-    def timer_callback(self):
-        msg = Vector3()
-        msg.x = self.velocity['joint1']
-        msg.y = self.velocity['joint2']
-        msg.z = self.velocity['joint3']
-        self.pub_goal.publish(msg)
-
-    def position_callback(self, msg):
-        self.get_logger().info('Current position: x=%f, y=%f, z=%f' % (msg.x, msg.y, msg.z))
-
-    def run(self):
-        while rclpy.ok():
-            rclpy.spin_once(self)
-            if select.select([sys.stdin], [], [], 0)[0] == [sys.stdin]:
-                key = sys.stdin.read(1)
-                if key == 'q':
-                    break
-                elif key == 'w':
-                    self.velocity['joint1'] = min(1.0, self.velocity['joint1'] + 0.1)
-                elif key == 's':
-                    self.velocity['joint1'] = max(-1.0, self.velocity['joint1'] - 0.1)
-                elif key == 'e':
-                    self.velocity['joint2'] = min(1.0, self.velocity['joint2'] + 0.1)
-                elif key == 'd':
-                    self.velocity['joint2'] = max(-1.0, self.velocity['joint2'] - 0.1)
-                elif key == 'r':
-                    self.velocity['joint3'] = min(1.0, self.velocity['joint3'] + 0.1)
-                elif key == 'f':
-                    self.velocity['joint3'] = max(-1.0, self.velocity['joint3'] - 0.1)
-                elif key == '1':
-                    msg = String()
-                    msg.data = 'zone1'
-                    self.pub_zone.publish(msg)
-                elif key == '2':
-                    msg = String()
-                    msg.data = 'zone2'
-                    self.pub_zone.publish(msg)
-                elif key == '3':
-                    msg = String()
-                    msg.data = 'zone3'
-                    self.pub_zone.publish(msg)
-
-        termios.tcsetattr(self.fd, termios.TCSADRAIN, self.old_settings)
-        self.get_logger().info('Teleop robot stopped')
-
+    def close_gripper(self):
+        # Close gripper with default step size of 2
+        gripper_step = -2
+        self.gripper_open = False
+        gripper_pos = gripper_step
+        gripper_msg = String(data=str(gripper_pos))
+        self.gripper_pub.publish(gripper_msg)
 
 def main(args=None):
     rclpy.init(args=args)
-    teleop_robot = TeleopRobot()
-    teleop_robot.run()
-    teleop_robot.destroy_node()
-    rclpy.shutdown()
+    joint_speeds = [0.5, 0.5, 0.5]  # Default joint speeds
+    robot_controller = RobotController(joint_speeds)
 
+    # Register keyboard events to control robot
+    keyboard.add_hotkey('up', lambda: robot_controller.update_joint_positions([1, 0, 0]))
+    keyboard.add_hotkey('down', lambda: robot_controller.update_joint_positions([-1, 0, 0]))
+    keyboard.add_hotkey('left', lambda: robot_controller.update_joint_positions([0, 1, 0]))
+    keyboard.add_hotkey('right', lambda: robot_controller.update_joint_positions([0, -1, 0]))
+    keyboard.add_hotkey('page up', lambda: robot_controller.update_joint_positions([0, 0, 1]))
+    keyboard.add_hotkey('page down', lambda: robot_controller.update_joint_positions([0, 0, -1]))
+    keyboard.add_hotkey('space', lambda: robot_controller.open_gripper())
+    keyboard.add_hotkey('enter', lambda: robot_controller.close_gripper())
 
-if __name__ == '__main__':
-    main()
+    rclpy.spin(robot_controller)
+    robot_controller.destroy
--- mi_robot_manipulador_2/build/mi_robot_manipulador_2/build/lib/mi_robot_manipulador_2/robot_manipulador_teleop_test.py
+++ mi_robot_manipulador_2/build/mi_robot_manipulador_2/build/lib/mi_robot_manipulador_2/robot_manipulador_teleop_test.py
@@ -1,24 +1,41 @@
-import rclpy
-from rclpy.node import Node
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
 from geometry_msgs.msg import Vector3
 
-class TestNode(Node):
-
-    def __init__(self):
-        super().__init__('test_node')
-        self.subscription = self.create_subscription(Vector3, '/robot_manipulador_goal', self.callback, 10)
-
-    def callback(self, msg):
-        self.get_logger().info('Received goal: x=%f, y=%f, z=%f' % (msg.x, msg.y, msg.z))
-
-
-def main(args=None):
-    rclpy.init(args=args)
-    test_node = TestNode()
-    rclpy.spin(test_node)
-    test_node.destroy_node()
-    rclpy.shutdown()
-
+def test_node():
+    rospy.init_node('test_node')
+    rate = rospy.Rate(10) # 10Hz
+    
+    pub_zone = rospy.Publisher('/robot_manipulator_zone', String, queue_size=10)
+    pub_position = rospy.Publisher('/robot_manipulator_position', Vector3, queue_size=10)
+    pub_goal = rospy.Publisher('/robot_manipulator_goal', Vector3, queue_size=10)
+    
+    zone_options = ["zone1", "zone2", "zone3"]
+    
+    while not rospy.is_shutdown():
+        # Publica mensajes en los tópicos
+        zone_msg = String()
+        zone_msg.data = zone_options[0] # Selecciona la primera zona
+        pub_zone.publish(zone_msg)
+        
+        position_msg = Vector3()
+        position_msg.x = 1.0 # Posición en x
+        position_msg.y = 2.0 # Posición en y
+        position_msg.z = 3.0 # Posición en z
+        pub_position.publish(position_msg)
+        
+        goal_msg = Vector3()
+        goal_msg.x = 4.0 # Posición deseada en x
+        goal_msg.y = 5.0 # Posición deseada en y
+        goal_msg.z = 6.0 # Posición deseada en z
+        pub_goal.publish(goal_msg)
+        
+        rate.sleep()
 
 if __name__ == '__main__':
-    main()
+    try:
+        test_node()
+    except rospy.ROSInterruptException:
+        pass
