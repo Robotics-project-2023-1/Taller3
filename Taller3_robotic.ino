1f78a5b9cd674153e93ce61f24678e4129df48a4
Yaisa C. Ramirez <81448945+yaisa5ramirez@users.noreply.github.com>
2023-05-20 21:57:54 -0500

    Movimiento de angulos generales, no por pasos

--- Taller3_robotic.ino
+++ Taller3_robotic.ino
@@ -2,66 +2,50 @@
 Servo motor1;
 Servo motor2;
 Servo motor3;
+Servo motor4;
 int anterior1;
 int anterior2;
 int anterior3;
+int anterior4;
 
 void setup()
 {
   Serial.begin(230400);
-  motor1.attach(8);
-  motor2.attach(9);
-  motor3.attach(10);
-  anterior1 = 30;
-  anterior2 = 40;
-  anterior3 = 165; //Garra cerrada
-  motor1.write(anterior1);
-  motor2.write(anterior2);
-  motor3.write(anterior3);
-  Serial.println(motor1.read());
-  Serial.println(motor2.read());
-  Serial.println(motor3.read());
+  motor1.attach(9);
+  motor2.attach(10);
+  motor3.attach(11);
+  motor4.attach(6);
+  motor1.write(90);
+  motor2.write(90);
+  motor3.write(90);
+  motor4.write(90); // Valores iniciales
 }
 
 void loop() {
   if (Serial.available() > 0) {
     // Read the incoming string
     String integer_string = Serial.readStringUntil('\n');
-    int integer_array[3];
+    int integer_array[4];
     int i = 0;
     char* token = strtok((char*)integer_string.c_str(), ",");
-    while (token != NULL && i < 3) {
+    while (token != NULL && i < 4) {
       integer_array[i] = atoi(token);
       i++;
       token = strtok(NULL, ",");
     }
-    if (integer_array[0] != 0 ) { //Si el comando es "on"
-
-      motor1.write(motor1.read() + integer_array[0]);
-      anterior1 = motor1.read();
-      Serial.println("MOTOR1 MUEVE");
-      Serial.println(motor1.read());
-
-    }
-    else if (integer_array[1] != 0 ) {
-      motor2.write(motor2.read() + integer_array[1]);
-      anterior2 = motor2.read();
-      Serial.println("MOTOR2 MUEVE");
-      Serial.println(motor2.read());
-
-
-    }
-    else if (integer_array[2] != 0 ) { // Funciona
-      motor3.write(motor3.read() + integer_array[2]);
-      anterior3 = motor3.read();
-      Serial.println("MOTOR3 MUEVE");
-        Serial.println(motor3.read());
-
-
-    }
-
+      motor1.write(integer_array[0]);
+      motor2.write(integer_array[1]);
+      motor3.write(integer_array[2]);
+      motor4.write(integer_array[3]);
     i = 0;
     Serial.flush();
   }
 
 }
+// r1 = 3.5cm
+// r2 = 8cm
+// r3 = 8cm
+// r4 = 3.5
+// r5 = 8cm
+// (x,y) = 8,8 inicial con 180, 90
+// Convertir mi x, yz, a polares y usar ese angulo como mi angulo de la base
