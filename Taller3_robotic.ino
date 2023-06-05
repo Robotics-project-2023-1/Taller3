1f206555d479a718a6a850198bd87f2283993325
Yaisa C. Ramirez <81448945+yaisa5ramirez@users.noreply.github.com>
2023-06-05 02:46:36 -0500

    Update Taller3_robotic.ino

--- Taller3_robotic.ino
+++ Taller3_robotic.ino
@@ -1,67 +1,60 @@
 #include <Servo.h>
+// Dato de entrada: [selector, motor1, motor2, eje1, eje2, eje3, eje4]
+
+// Pines Brazo
+#define Servo1_pin 8 // PWM
+#define Servo2_pin 9 // PWM
+#define Servo3_pin 10 // PWM
+#define Servo4_pin 11 // PWM
 Servo motor1;
 Servo motor2;
 Servo motor3;
-int anterior1;
-int anterior2;
-int anterior3;
+Servo motor4;
+
+// Variable brazo
+int inicial1 = 90;
+int inicial2 = 90;
+int inicial3 = 90;
+int inicial4 = 90;
+
+// Funciones generales --------------------------------------------------------
 
 void setup()
 {
+  // Protocolos de comunicacion
   Serial.begin(230400);
-  motor1.attach(8);
-  motor2.attach(9);
-  motor3.attach(10);
-  anterior1 = 30;
-  anterior2 = 40;
-  anterior3 = 165; //Garra cerrada
-  motor1.write(anterior1);
-  motor2.write(anterior2);
-  motor3.write(anterior3);
-  Serial.println(motor1.read());
-  Serial.println(motor2.read());
-  Serial.println(motor3.read());
+  // Brazo
+  motor1.attach(Servo1_pin);
+  motor2.attach(Servo2_pin);
+  motor3.attach(Servo3_pin);
+  motor4.attach(Servo4_pin);
+  motor1.write(inicial1);
+  motor2.write(inicial2);
+  motor3.write(inicial3);
+  motor4.write(inicial4);
 }
 
 void loop() {
+
   if (Serial.available() > 0) {
     // Read the incoming string
     String integer_string = Serial.readStringUntil('\n');
-    int integer_array[3];
+    int integer_array[7]; //motor1, motor2, Servo1, servo2, servo3, servo4
     int i = 0;
     char* token = strtok((char*)integer_string.c_str(), ",");
-    while (token != NULL && i < 3) {
+    while (token != NULL && i < 7) {
       integer_array[i] = atoi(token);
       i++;
       token = strtok(NULL, ",");
     }
-    if (integer_array[0] != 0 ) { //Si el comando es "on"
-
-      motor1.write(motor1.read() + integer_array[0]);
-      anterior1 = motor1.read();
-      Serial.println("MOTOR1 MUEVE");
-      Serial.println(motor1.read());
-
-    }
-    else if (integer_array[1] != 0 ) {
-      motor2.write(motor2.read() + integer_array[1]);
-      anterior2 = motor2.read();
-      Serial.println("MOTOR2 MUEVE");
-      Serial.println(motor2.read());
-
-
-    }
-    else if (integer_array[2] != 0 ) { // Funciona
-      motor3.write(motor3.read() + integer_array[2]);
-      anterior3 = motor3.read();
-      Serial.println("MOTOR3 MUEVE");
-        Serial.println(motor3.read());
-
-
+    if (integer_array[0] == 2) { // Mover brazo
+      motor1.write(integer_array[3]);
+      motor2.write(integer_array[4]);
+      motor3.write(integer_array[5]);
+      motor4.write(integer_array[6]);
     }
 
-    i = 0;
-    Serial.flush();
   }
+  i = 0;
 
-}
+  }
