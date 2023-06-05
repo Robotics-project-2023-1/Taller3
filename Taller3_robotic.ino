37940d19449ab1e796fc2ef7c0b7ccc6f8d257d5
Yaisa C. Ramirez <81448945+yaisa5ramirez@users.noreply.github.com>
2023-06-05 03:05:09 -0500

    Update Taller3_robotic.ino

--- Taller3_robotic.ino
+++ Taller3_robotic.ino
@@ -18,6 +18,9 @@ int inicial3 = 90;
 int inicial4 = 90;
 
 // Funciones generales --------------------------------------------------------
+int pasos1_ant = inicial1;
+int pasos2_ant = inicial2;
+int pasos3_ant = inicial3;
 
 void setup()
 {
@@ -39,22 +42,25 @@ void loop() {
   if (Serial.available() > 0) {
     // Read the incoming string
     String integer_string = Serial.readStringUntil('\n');
-    int integer_array[7]; //motor1, motor2, Servo1, servo2, servo3, servo4
+    int integer_array[6]; //motor1, motor2, Servo1, servo2, servo3, servo4
     int i = 0;
     char* token = strtok((char*)integer_string.c_str(), ",");
-    while (token != NULL && i < 7) {
+    while (token != NULL && i < 6) {
       integer_array[i] = atoi(token);
       i++;
       token = strtok(NULL, ",");
     }
     if (integer_array[0] == 2) { // Mover brazo
-      motor1.write(integer_array[3]);
-      motor2.write(integer_array[4]);
-      motor3.write(integer_array[5]);
+      motor1.write(pasos1_ant + integer_array[3]);
+      motor2.write(pasos2_ant + integer_array[4]);
+      motor3.write(pasos3_ant + integer_array[5]);
       motor4.write(integer_array[6]);
+      pasos1_ant = pasos1_ant + integer_array[3];
+      pasos2_ant = pasos2_ant + integer_array[3];
+      pasos3_ant = pasos3_ant + integer_array[3];
     }
-
-  }
   i = 0;
 
+  }
+  
   }
